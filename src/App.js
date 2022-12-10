d2fa2df66b22b16131ec97fc8b280f8fccdb9d8c
rakib38324 <rakib3832@gmail.com>
2022-12-10 13:46:13 +0600

    Commit the icon and navber section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>welcome</h1>
+    <div className='max-w-7xl mx-auto'>
+      <Home></Home>
     </div>
   );
 }
